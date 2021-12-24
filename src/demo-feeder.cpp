2c73a4a771a92d3fbd6dc8458c36a07b8c73b6cd
Fred Emmott <git@fredemmott.com>
2021-12-23 20:12:48 -0600

    On knee

--- src/demo-feeder.cpp
+++ src/demo-feeder.cpp
@@ -1,6 +1,9 @@
-#include "YAVRK/SHM.h"
-
+#define _USE_MATH_DEFINES
 #include <Windows.h>
+
+#include <cmath>
+
+#include "YAVRK/SHM.h"
 
 int main() {
   // - the data format is RGBA
@@ -15,9 +18,20 @@ int main() {
 
   YAVRK::SHMHeader config {
     .Version = YAVRK::IPC_VERSION,
+    /* Headlocked
     .Flags = YAVRK::Flags::HEADLOCKED | YAVRK::Flags::DISCARD_DEPTH_INFORMATION,
     .y = -0.15,
     .z = -0.5f,
+    .VirtualWidth = 0.2f,
+    .VirtualHeight = 0.3f,
+    .ImageWidth = 400,
+    .ImageHeight = 1200,
+    */
+    /* On knee */
+    .Flags = YAVRK::Flags::DISCARD_DEPTH_INFORMATION,
+    .y = 0.5,
+    .z = -0.25f,
+    .rx = M_PI / 2,
     .VirtualWidth = 0.2f,
     .VirtualHeight = 0.3f,
     .ImageWidth = 400,
@@ -30,9 +44,9 @@ int main() {
     uint32_t color;
     for (auto y = 0; y < config.ImageHeight; y++) {
       for (auto x = 0; x < config.ImageWidth; x++) {
-        if (y < config.ImageHeight/ 4) {
+        if (y < config.ImageHeight / 4) {
           color = colors[frames % 4];
-        } else if (y < config.ImageHeight/ 2) {
+        } else if (y < config.ImageHeight / 2) {
           color = colors[(frames + 1) % 4];
         } else if (y < 3 * config.ImageHeight / 4) {
           color = colors[(frames + 2) % 4];
