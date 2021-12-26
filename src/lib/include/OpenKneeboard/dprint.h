18d5b3a5d046c57c08c57a91a2ad6982639ef91c
Fred Emmott <git@fredemmott.com>
2021-12-26 12:18:41 -0600

    Make dprint default target depend on build mode

--- src/lib/include/OpenKneeboard/dprint.h
+++ src/lib/include/OpenKneeboard/dprint.h
@@ -8,20 +8,27 @@ void dprint(const std::string& s);
 
 template <typename... T>
 void dprintf(fmt::format_string<T...> fmt, T&&... args) {
-	dprint(fmt::format(fmt, std::forward<T>(args)...));
+  dprint(fmt::format(fmt, std::forward<T>(args)...));
 }
 
 struct DPrintSettings {
-	enum class Target {
-		// Traditional console window, created if necessary
-		CONSOLE,
-		// Use debugger or `dbgview` etc to view this
-		DEBUG_STREAM,
-	};
-	std::string Prefix = "OpenKneeboard";
-	Target Target = Target::DEBUG_STREAM;
+  enum class Target {
+    // Traditional console window, created if necessary
+    CONSOLE,
+    // Use debugger or `dbgview` etc to view this
+    DEBUG_STREAM,
+  };
+  static const Target DEFAULT_TARGET =
+#ifdef DEBUG
+    Target::CONSOLE;
+#else
+    Target::DEBUG_STREAM;
+#endif
 
-	static void Set(const DPrintSettings&);
+  std::string Prefix = "OpenKneeboard";
+  Target Target = DEFAULT_TARGET;
+
+  static void Set(const DPrintSettings&);
 };
 
-}
+}// namespace OpenKneeboard
