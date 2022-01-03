91f536083b5ff11628613ccea1106ccd1d860390
Fred Emmott <git@fredemmott.com>
2022-01-02 18:10:43 -0600

    Add intersection test
    Will be used for zooming

--- src/injectables/OculusKneeboard.cpp
+++ src/injectables/OculusKneeboard.cpp
@@ -14,11 +14,55 @@
 // clang-format on
 
 #define LIBOVR_FUNCS \
+  IT(ovr_GetTrackingState) \
+  IT(ovr_GetPredictedDisplayTime) \
   IT(ovr_GetTrackingOriginType)
 
 #define IT(x) static decltype(&x) real_##x = nullptr;
 LIBOVR_FUNCS
 #undef IT
+
+using namespace OpenKneeboard;
+
+static bool poseIntersectsWithRect(
+  const ovrPosef& pose,
+  const ovrPosef& rectCenter,
+  const ovrVector2f& rectSize) {
+  const auto rayOrigin = OVR::Vector3f(pose.Position);
+  const auto rayNormal = OVR::Quatf(pose.Orientation) * OVR::Vector3f(0, 0, -1);
+  const auto planeQuat = OVR::Quatf(rectCenter.Orientation);
+  const auto planeOrigin = OVR::Vector3f(rectCenter.Position);
+  const auto planeNormal = planeQuat * OVR::Vector3f(0, 0, 1);
+
+  // Does the ray intersect the infinite plane?
+  if (rayNormal.Dot(planeNormal) >= 0) {
+    return false;
+  }
+
+  // Where does it intersect the infinite plane?
+  const auto rayOriginToPlaneOrigin = planeOrigin - rayOrigin;
+  const auto rayLength
+    = (planeOrigin - rayOrigin).Dot(planeNormal) / rayNormal.Dot(planeNormal);
+  const auto worldPoint = rayOrigin + (rayNormal * rayLength);
+
+  /// Finally: is the intersection point within the rectangle?
+  const auto point = worldPoint - planeOrigin;
+
+  const auto xVector = planeQuat * OVR::Vector3f(1, 0, 0);
+  const auto x = point.Dot(xVector);
+
+  if (abs(x) > rectSize.x / 2) {
+    return false;
+  }
+
+  const auto yVector = planeQuat * OVR::Vector3f(0, 1, 0);
+  const auto y = point.Dot(yVector);
+  if (abs(y) > rectSize.y / 2) {
+    return false;
+  }
+
+  return true;
+}
 
 namespace OpenKneeboard {
 
@@ -75,6 +119,14 @@ ovrResult OculusKneeboard::onEndFrame(
   kneeboardLayer.Viewport.Pos = {.x = 0, .y = 0};
   kneeboardLayer.Viewport.Size
     = {.w = config.ImageWidth, .h = config.ImageHeight};
+
+  auto predictedTime = real_ovr_GetPredictedDisplayTime(session, frameIndex);
+  auto state = real_ovr_GetTrackingState(session, predictedTime, false);
+
+  auto intersects = poseIntersectsWithRect(
+    state.HeadPose.ThePose,
+    kneeboardLayer.QuadPoseCenter,
+    kneeboardLayer.QuadSize);
 
   std::vector<const ovrLayerHeader*> newLayers;
   if (layerCount == 0) {
