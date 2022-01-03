77b7357d42cc3ccf02b6679d6c83a6395a74b50f
Fred Emmott <git@fredemmott.com>
2022-01-02 20:55:44 -0600

    Improve RadioLogTab rendering/performance/bugs

--- src/tabs/DCSRadioLogTab.cpp
+++ src/tabs/DCSRadioLogTab.cpp
@@ -16,11 +16,9 @@ void DCSRadioLogTab::Reload() {
 }
 
 uint16_t DCSRadioLogTab::GetPageCount() const {
-  if (mMessages.empty()) {
-    return 0;
-  }
   return 1;
 }
+
 wxImage DCSRadioLogTab::RenderPage(uint16_t index) {
   if (index != 0) {
     return {};
@@ -31,28 +29,28 @@ wxImage DCSRadioLogTab::RenderPage(uint16_t index) {
 
   dc.SetFont(
     wxFont(16, wxFONTFAMILY_TELETYPE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+  dc.SetBackground(*wxWHITE_BRUSH);
+  dc.Clear();
 
   auto metrics = dc.GetTextExtent("m");
   auto padding = metrics.GetHeight();
-  auto columns = (bitmap.GetWidth() - (2 * padding)/ metrics.GetWidth());
-  auto rows = (bitmap.GetHeight() - (2 * padding) / metric.GetHeight());
+  const auto columns = (bitmap.GetWidth() - (2 * padding)) / metrics.GetWidth();
+  const auto rows = (bitmap.GetHeight() - (2 * padding)) / metrics.GetHeight();
 
   if (mMessages.empty()) {
-    dc.SetTextForeground(wxColor(0x99, 0x99, 0x99));
-    dc.DrawText(_("[waiting for radio messages]"));
+    dc.SetTextForeground(wxColor(0x55, 0x55, 0x55));
+    dc.DrawText(_("[waiting for radio messages]"), wxPoint(padding, padding));
     return bitmap.ConvertToImage();
   }
 
   std::vector<std::string_view> lines;
   for (const auto& message: mMessages) {
-    if (message.size() <= columns) {
-      lines.push_back(message);
-      lines.push_back({});
-      continue;
-    }
-
     std::string_view remaining(message);
     while (!remaining.empty()) {
+      if (remaining.size() <= columns) {
+        lines.push_back(remaining);
+        break;
+      }
       auto space = remaining.find_last_of(" ", columns);
       if (space == remaining.npos) {
         lines.push_back(remaining.substr(0, columns));
@@ -66,20 +64,18 @@ wxImage DCSRadioLogTab::RenderPage(uint16_t index) {
       lines.push_back(remaining.substr(0, space));
       if (remaining.size() > space + 1) {
         remaining = remaining.substr(space + 1);
+      } else {
+        break;
       }
     }
     lines.push_back({});
   }
 
-  auto rows = (bitmap.GetHeight() / metrics.GetHeight() - 2);
   if (lines.size() > rows) {
     lines = {lines.begin() + (lines.size() - rows), lines.end()};
   }
 
-  dc.SetBackground(*wxWHITE_BRUSH);
-  dc.Clear();
-
-  wxPoint point {metrics.GetWidth(), metrics.GetHeight()};
+  wxPoint point {padding, padding};
   auto y = metrics.GetHeight();
   for (const auto& line: lines) {
     dc.DrawText(wxString(line.data(), line.size()), point);
@@ -98,7 +94,6 @@ void DCSRadioLogTab::Update(
   const std::filesystem::path& savedGamesPath,
   const std::string& value) {
   mMessages.push_back(value);
-  wxQueueEvent(this, new wxCommandEvent(okEVT_TAB_UPDATED));
 }
 
 }// namespace OpenKneeboard
