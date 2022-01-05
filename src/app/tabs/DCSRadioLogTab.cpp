8491c130d50d29426824b7e6669b959ff1a89453
Fred Emmott <git@fredemmott.com>
2022-01-04 22:34:30 -0600

    Don't set font to UTF8
    wxWidgets is unable to find a usable font

--- src/app/tabs/DCSRadioLogTab.cpp
+++ src/app/tabs/DCSRadioLogTab.cpp
@@ -34,10 +34,8 @@ wxImage DCSRadioLogTab::RenderPage(uint16_t index) {
   wxBitmap bitmap(768, 1024);
   wxMemoryDC dc(bitmap);
 
-  wxFont font(
-    16, wxFONTFAMILY_TELETYPE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
-  font.SetEncoding(wxFONTENCODING_UTF8);
-  dc.SetFont(font);
+  dc.SetFont(
+    wxFont(16, wxFONTFAMILY_TELETYPE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
   dc.SetBackground(*wxWHITE_BRUSH);
   dc.Clear();
 
