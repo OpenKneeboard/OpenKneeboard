4864f91eef5bfae6665d86f0ef75cd38b98f41f8
Fred Emmott <git@fredemmott.com>
2022-01-08 15:28:09 -0600

    Copy DCS extension to bin dir too
    </3 cmake

--- src/app/RuntimeFiles.h
+++ src/app/RuntimeFiles.h
@@ -4,6 +4,8 @@
 
 namespace OpenKneeboard::RuntimeFiles {
 
+extern const std::filesystem::path DCSWORLD_HOOK_DLL;
+extern const std::filesystem::path DCSWORLD_HOOK_LUA;
 extern const std::filesystem::path OCULUS_D3D11_DLL;
 
 }
