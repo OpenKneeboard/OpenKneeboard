9c62f882c956e885d2f1288bc52af98e21b3cf42
Fred Emmott <git@fredemmott.com>
2022-01-08 16:25:17 -0600

    Also prompt to update outdated DCS hooks

--- src/app/tabs/DCSTab.cpp
+++ src/app/tabs/DCSTab.cpp
@@ -1,5 +1,9 @@
 #include "OpenKneeboard/DCSTab.h"
 
+#include <fmt/xchar.h>
+#include <wx/msgdlg.h>
+
+#include <algorithm>
 #include <filesystem>
 
 #include "OpenKneeboard/GameEvent.h"
@@ -7,11 +11,51 @@
 #include "OpenKneeboard/dprint.h"
 #include "RuntimeFiles.h"
 
-#include <fmt/xchar.h>
-#include <wx/msgdlg.h>
-
 using DCS = OpenKneeboard::Games::DCSWorld;
 using namespace OpenKneeboard;
+
+static bool FilesDiffer(
+  const std::filesystem::path& a,
+  const std::filesystem::path& b) {
+  const auto size = std::filesystem::file_size(a);
+  if (std::filesystem::file_size(b) != size) {
+    return true;
+  }
+
+  winrt::handle ah {CreateFile(
+    a.c_str(),
+    GENERIC_READ,
+    FILE_SHARE_READ | FILE_SHARE_WRITE,
+    nullptr,
+    OPEN_EXISTING,
+    FILE_ATTRIBUTE_NORMAL,
+    NULL)};
+  winrt::handle bh {CreateFile(
+    b.c_str(),
+    GENERIC_READ,
+    FILE_SHARE_READ | FILE_SHARE_WRITE,
+    nullptr,
+    OPEN_EXISTING,
+    FILE_ATTRIBUTE_NORMAL,
+    NULL)};
+
+  winrt::handle afm {
+    CreateFileMappingW(ah.get(), nullptr, PAGE_READONLY, 0, 0, nullptr)};
+  winrt::handle bfm {
+    CreateFileMappingW(bh.get(), nullptr, PAGE_READONLY, 0, 0, nullptr)};
+
+  auto av = reinterpret_cast<std::byte*>(
+    MapViewOfFile(afm.get(), FILE_MAP_READ, 0, 0, 0));
+  auto bv = reinterpret_cast<std::byte*>(
+    MapViewOfFile(bfm.get(), FILE_MAP_READ, 0, 0, 0));
+
+  auto equal = std::equal(av, av + size, bv, bv + size);
+
+  UnmapViewOfFile(av);
+  UnmapViewOfFile(bv);
+
+  return !equal;
+}
 
 static void InstallHooks(DCS::Version version, const wxString& label) {
   const auto baseDir = DCS::GetSavedGamesPath(version);
@@ -23,28 +67,42 @@ static void InstallHooks(DCS::Version version, const wxString& label) {
   const auto dllDest = hookDir / RuntimeFiles::DCSWORLD_HOOK_DLL;
   const auto luaDest = hookDir / RuntimeFiles::DCSWORLD_HOOK_LUA;
 
-  if (std::filesystem::exists(dllDest) && std::filesystem::exists(luaDest)) {
-    return;
-  }
-
-  const auto message = fmt::format(
-    _("Required hooks aren't installed for {}; would you like to install them?").ToStdWstring(),
-    label.ToStdWstring());
-  
-  wxMessageDialog dialog(nullptr, message, "OpenKneeboard", wxOK | wxCANCEL | wxICON_WARNING);
-  if (dialog.ShowModal() != wxID_OK) {
-    return;
-  }
-
   wchar_t buffer[MAX_PATH];
   GetModuleFileNameW(NULL, buffer, MAX_PATH);
   const auto exeDir = std::filesystem::path(buffer).parent_path();
   const auto dllSource = exeDir / RuntimeFiles::DCSWORLD_HOOK_DLL;
   const auto luaSource = exeDir / RuntimeFiles::DCSWORLD_HOOK_LUA;
 
+  std::wstring message;
+  if (!(std::filesystem::exists(dllDest) && std::filesystem::exists(luaDest))) {
+    message = fmt::format(
+      _("Required hooks aren't installed for {}; would you like to install "
+        "them?")
+        .ToStdWstring(),
+      label.ToStdWstring());
+  } else if (
+    FilesDiffer(dllSource, dllDest) || FilesDiffer(luaSource, luaDest)) {
+    message = fmt::format(
+      _("Hooks for {} are out of date; would you like to update "
+        "them?")
+        .ToStdWstring(),
+      label.ToStdWstring());
+  } else {
+    // Installed and equal
+    return;
+  }
+
+  wxMessageDialog dialog(
+    nullptr, message, "OpenKneeboard", wxOK | wxCANCEL | wxICON_WARNING);
+  if (dialog.ShowModal() != wxID_OK) {
+    return;
+  }
+
   std::filesystem::create_directories(hookDir);
-  std::filesystem::copy_file(dllSource, dllDest);
-  std::filesystem::copy_file(luaSource, luaDest);
+  std::filesystem::copy_file(
+    dllSource, dllDest, std::filesystem::copy_options::overwrite_existing);
+  std::filesystem::copy_file(
+    luaSource, luaDest, std::filesystem::copy_options::overwrite_existing);
 }
 
 static void InstallHooks() {
