c695dbbce96af118709c57cf98d362d156b7c5d5
Fred Emmott <git@fredemmott.com>
2022-01-08 20:15:03 -0600

    Detect OpenVR
    Needed so that we can tell the  difference between VR and flatscreen

--- src/injectables/AutoDetectKneeboard.cpp
+++ src/injectables/AutoDetectKneeboard.cpp
@@ -15,8 +15,10 @@ namespace OpenKneeboard {
 class AutoDetectKneeboard final : private OculusFrameHook,
                                   private IDXGISwapChainPresentHook {
  private:
-  const uint64_t FLAG_OCULUS = 1 << 0;
-  const uint64_t FLAG_D3D11 = 1 << 1;
+  const uint64_t FLAG_D3D11 = 1ui64 << 0;
+  const uint64_t FLAG_OCULUS = 1ui64 << 32;
+  const uint64_t FLAG_STEAMVR = 1ui64 << 33;
+
   uint64_t mFlags = 0;
   uint64_t mFrames = 0;
 
@@ -70,12 +72,27 @@ class AutoDetectKneeboard final : private OculusFrameHook,
   void Next() {
     Unhook();
 
+    CheckForSteamVR();
+
+    if ((mFlags & FLAG_STEAMVR)) {
+      dprint("Doing nothing as SteamVR is in-process");
+      return;
+    }
+
     if (mFlags == (FLAG_D3D11 | FLAG_OCULUS)) {
       mNext = std::make_unique<OculusD3D11Kneeboard>();
       return;
     }
 
     dprintf("Don't know how to create a kneeboard from autodetection flags {:#b}", mFlags);
+  }
+
+  void CheckForSteamVR() {
+    // Not using OpenVR as we would need to load openvr_api.dll in process
+    auto f = DetourFindFunction("vrclient_x64.dll", "VRClientCoreFactory");
+    if (f) {
+      mFlags |= FLAG_STEAMVR;
+    }
   }
 };
 
