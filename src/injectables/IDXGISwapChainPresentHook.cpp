379d33dec87699bb6692b732476ad1413a2de661
Fred Emmott <git@fredemmott.com>
2022-01-11 10:45:31 -0600

    Basic detection of Steam overlay
    Needed as it interferes with our own hooks

--- src/injectables/IDXGISwapChainPresentHook.cpp
+++ src/injectables/IDXGISwapChainPresentHook.cpp
@@ -2,7 +2,10 @@
 
 #include <OpenKneeboard/dprint.h>
 #include <d3d11.h>
+#include <psapi.h>
 #include <winrt/base.h>
+
+#include <utility>
 
 #include "detours-ext.h"
 #include "dxgi-offsets.h"
@@ -16,6 +19,148 @@ bool gHooked = false;
 uint16_t gCount = 0;
 
 decltype(&IDXGISwapChain::Present) Real_IDXGISwapChain_Present = nullptr;
+
+std::vector<std::pair<uint64_t, uint64_t>> ComputeFuncPatterns(
+  const std::basic_string_view<unsigned char>& rawPattern) {
+  std::vector<std::pair<uint64_t, uint64_t>> patterns;
+  auto view(rawPattern);
+  while (!view.empty()) {
+    std::string pattern(8, '\0');
+    std::string mask(8, '\0');
+    for (auto i = 0; i < 8 && i < view.size(); ++i) {
+      if (view[i] != '?') {
+        pattern[i] = view[i];
+        mask[i] = 0xffi8;
+      }
+    }
+    patterns.push_back(
+      {*(uint64_t*)(pattern.data()), *(uint64_t*)(mask.data())});
+
+    if (view.size() <= 8) {
+      break;
+    }
+    view = view.substr(8);
+  }
+
+  dprint("Code search pattern:");
+  for (auto& pattern: patterns) {
+    dprintf(
+      FMT_STRING("{:016x} (mask {:016x})"),
+      _byteswap_uint64(pattern.first),
+      _byteswap_uint64(pattern.second));
+  }
+
+  return patterns;
+}
+
+void* FindFuncPattern(
+  const std::vector<std::pair<uint64_t, uint64_t>>& allPatterns,
+  void* _begin,
+  void* _end) {
+  auto begin = reinterpret_cast<uint64_t*>(_begin);
+  auto end = reinterpret_cast<uint64_t*>(_end);
+  dprintf(
+    FMT_STRING("Code search range: {:#018x}-{:#018x}"),
+    (uint64_t)begin,
+    (uint64_t)end);
+
+  const uint64_t firstPattern = allPatterns.front().first,
+                 firstMask = allPatterns.front().second;
+  auto patterns = allPatterns;
+  patterns.erase(patterns.begin());
+  // Stack entries (including functions) are always aligned on 16-byte
+  // boundaries
+  for (auto func = begin; func < end; func += (16 / sizeof(*func))) {
+    auto it = func;
+    if ((*it & firstMask) != firstPattern) {
+      continue;
+    }
+    for (auto& pattern: patterns) {
+      it++;
+      if (it >= end) {
+        return nullptr;
+      }
+
+      if ((*it & pattern.second) != pattern.first) {
+        goto FindFuncPattern_NextBlock;
+      }
+    }
+    return reinterpret_cast<void*>(func);
+
+  FindFuncPattern_NextBlock:
+    continue;
+  }
+
+  return nullptr;
+}
+
+void* FindFuncPatternInModule(
+  const char* moduleName,
+  const std::basic_string_view<unsigned char>& rawPattern,
+  bool* foundMultiple) {
+  auto hModule = GetModuleHandleA(moduleName);
+  if (!hModule) {
+    dprintf("Module {} is not loaded.", moduleName);
+    return nullptr;
+  }
+  MODULEINFO info;
+  if (!GetModuleInformation(
+        GetCurrentProcess(), hModule, &info, sizeof(info))) {
+    dprintf("Failed to GetModuleInformation() for {}", moduleName);
+    return 0;
+  }
+
+  auto begin = info.lpBaseOfDll;
+  auto end = reinterpret_cast<void*>(
+    reinterpret_cast<std::byte*>(info.lpBaseOfDll) + info.SizeOfImage);
+  auto pattern = ComputeFuncPatterns(rawPattern);
+  auto addr = FindFuncPattern(pattern, begin, end);
+  if (addr == nullptr || foundMultiple == nullptr) {
+    return addr;
+  }
+
+  auto nextAddr = reinterpret_cast<uintptr_t>(addr)
+    + (pattern.size() * sizeof(pattern.front().first));
+  // 16-byte alignment for all stack addresses
+  nextAddr -= (nextAddr % 16);
+  begin = reinterpret_cast<void*>(nextAddr);
+  if (FindFuncPattern(pattern, begin, end)) {
+    *foundMultiple = true;
+  }
+
+  return addr;
+}
+
+void* Find_SteamOverlay_IDXGI_SwapChain_Present() {
+  // We're trying to find a non-exported function, so we need to try and figure
+  // out where it is based on what it looks like.
+  // clang-format off
+  const unsigned char pattern[] = {
+    // Looking for the function prologue: save callee-preserved
+    // registers that the function uses; these are likely used
+    // by Steam's trampoline calling convention
+    0x48, 0x89, 0x6c, 0x24, '?', // MOV qword (stack offset) RBP
+    0x48, 0x89, 0x74, 0x24, '?', // MOV qword (stack offset) RSI
+    0x41, 0x56, // PUSH R14
+    // ... then adjust the stack by the fixed allocation size
+    0x48, 0x83, 0xec, '?', // SUB RSP, (fixed allocation size)
+    // End prologue: start doing stuff
+    0x41, 0x8b, 0xe8, // MOV EBP, R8D (arg3: UINT Flags)
+    0x8b, 0xf2, // MOV ESI, EDX (arg2: UINT SyncInterval)
+    0x4c, 0x8b, 0xf1, // MOV R14 (arg1: IDXGISwapChain* this)
+    0x41, 0xf6, 0xc0, 0x01 // TEST EBP,0x1 // TEST flags & DXGI_PRESENT_TEST
+  };
+  // clang-format on
+  bool foundMultiple = false;
+  dprint("Looking for SteamVR overlay");
+  auto func = FindFuncPatternInModule(
+    "GameOverlayRenderer64", {pattern, sizeof(pattern)}, &foundMultiple);
+  if (foundMultiple) {
+    dprintf("Found multiple potential Steam overlay functions :'(");
+    return nullptr;
+  }
+  return func;
+}
 
 }// namespace
 
@@ -52,8 +197,17 @@ void IDXGISwapChainPresentHook::Unhook() {
 }
 
 IDXGISwapChainPresentHook::IDXGISwapChainPresentHook() {
+  dprint(__FUNCTION__);
   if (gHook) {
     throw std::logic_error("Only one IDXGISwapChainPresentHook at a time!");
+  }
+
+  auto addr = Find_SteamOverlay_IDXGI_SwapChain_Present();
+  if (addr) {
+    dprintf("Found Steam Overlay hook at {:#016x}", (int64_t)addr);
+    return;
+  } else {
+    dprint("Did not find Steam Overlay hook");
   }
 
   gHook = this;
@@ -97,16 +251,16 @@ IDXGISwapChainPresentHook::IDXGISwapChainPresentHook() {
     device.put(),
     nullptr,
     nullptr);
-  dprintf(" - got a temporary device at {:#010x}", (intptr_t)device.get());
+  dprintf(" - got a temporary device at {:#018x}", (intptr_t)device.get());
   dprintf(
-    " - got a temporary SwapChain at {:#010x}", (intptr_t)swapchain.get());
+    " - got a temporary SwapChain at {:#018x}", (intptr_t)swapchain.get());
 
   auto fpp = reinterpret_cast<void**>(&Real_IDXGISwapChain_Present);
   *fpp = VTable_Lookup_IDXGISwapChain_Present(swapchain.get());
-  dprintf(" - found IDXGISwapChain::Present at {:#010x}", (intptr_t)*fpp);
+  dprintf(" - found IDXGISwapChain::Present at {:#018x}", (intptr_t)*fpp);
   auto mfp = &Impl::Hooked_IDXGISwapChain_Present;
   dprintf(
-    "Hooking &{:#010x} to {:#010x}",
+    "Hooking &{:#018x} to {:#018x}",
     (intptr_t)*fpp,
     (intptr_t) * (reinterpret_cast<void**>(&mfp)));
   DetourTransactionPushBegin();
