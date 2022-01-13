2b70b6375d23c577c8cec67bc162ea7cfb36a5a5
Fred Emmott <git@fredemmott.com>
2022-01-12 21:06:32 -0600

    Do a debug build of detours for debug builds of OpenKneeboard

--- third-party/detours.cmake
+++ third-party/detours.cmake
@@ -6,7 +6,10 @@ ExternalProject_Add(
   BUILD_IN_SOURCE ON
   WORKING_DIRECTORY "<SOURCE_DIR>/src"
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND "nmake"
+  BUILD_COMMAND
+    ${CMAKE_COMMAND} -E env
+    "DETOURS_CONFIG=$<IF:$<CONFIG:Debug>,Debug,Release>"
+    nmake
   INSTALL_COMMAND ""
 
   EXCLUDE_FROM_ALL
@@ -17,4 +20,4 @@ add_dependencies(detours detoursBuild)
 
 ExternalProject_Get_property(detoursBuild SOURCE_DIR)
 target_include_directories(detours INTERFACE ${SOURCE_DIR}/include)
-target_link_libraries(detours INTERFACE ${SOURCE_DIR}/lib.X64/detours.lib)
+target_link_libraries(detours INTERFACE "${SOURCE_DIR}/lib.X64$<$<CONFIG:Debug>:Debug>/detours.lib")
