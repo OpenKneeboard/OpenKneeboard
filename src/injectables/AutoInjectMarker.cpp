0f37bd4b05932beb7f32409f74d157fd03fc7246
Fred Emmott <git@fredemmott.com>
2022-01-15 14:41:57 -0600

    Add comment in autoInjectmarker

--- src/injectables/AutoInjectMarker.cpp
+++ src/injectables/AutoInjectMarker.cpp
@@ -1,5 +1,10 @@
 #include <Windows.h>
 
+/** This DLL does nothing; it exists just so the main app can already
+ * tell if it's injected a specific process.
+ *
+ * This is useful as the bootstrapper unloads itself once it's done.
+ */
 BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved) {
   return true;
 }
