9c1cc9e9d40303817a0b7e4fbe5f4a0bd020e5a6
Fred Emmott <git@fredemmott.com>
2022-02-14 10:10:41 -0600

    Initialize dprint settings on main app startup

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -14,18 +14,23 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  */
+#include <OpenKneeboard/SHM.h>
+#include <OpenKneeboard/dprint.h>
 #include <shims/wx.h>
 
 #include "okMainWindow.h"
-
-#include <OpenKneeboard/SHM.h>
 
 class OpenKneeboardApp final : public wxApp {
  public:
   virtual bool OnInit() override {
     {
+      OpenKneeboard::DPrintSettings::Set({
+        .prefix = "OpenKneeboard",
+      });
+
       // If already running, make the other instance active.
       //
       // There's more common ways to do this, but given we already have the SHM
