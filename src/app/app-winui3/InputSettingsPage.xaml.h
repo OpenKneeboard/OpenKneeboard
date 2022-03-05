6455b8a5891291e48ddc42ce81bcecc48b15405e
Fred Emmott <git@fredemmott.com>
2022-03-05 13:50:04 -0600

    Add skeleton input settings page

---
+++ src/app/app-winui3/InputSettingsPage.xaml.h
@@ -0,0 +1,40 @@
+/*
+ * OpenKneeboard
+ *
+ * Copyright (C) 2022 Fred Emmott <fred@fredemmott.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+#pragma once
+
+// clang-format off
+#include "pch.h"
+// clang-format on
+
+#include <string>
+
+#include "InputSettingsPage.g.h"
+
+using namespace winrt::Microsoft::UI::Xaml;
+
+namespace winrt::OpenKneeboardApp::implementation {
+struct InputSettingsPage : InputSettingsPageT<InputSettingsPage> {
+  InputSettingsPage();
+};
+}// namespace winrt::OpenKneeboardApp::implementation
+namespace winrt::OpenKneeboardApp::factory_implementation {
+struct InputSettingsPage
+  : InputSettingsPageT<InputSettingsPage, implementation::InputSettingsPage> {};
+}// namespace winrt::OpenKneeboardApp::factory_implementation
