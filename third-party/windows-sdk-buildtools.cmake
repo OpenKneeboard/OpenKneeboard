9bb5751b2795cca7aec4c6cd3126b39e66a2e093
Fred Emmott <git@fredemmott.com>
2022-03-10 20:05:15 -0600

    Explicitly get an absolute path to signtool

--- third-party/windows-sdk-buildtools.cmake
+++ third-party/windows-sdk-buildtools.cmake
@@ -17,4 +17,5 @@ ExternalProject_Add(
 
 ExternalProject_Get_property(WindowsSDKBuildToolsNuget SOURCE_DIR)
 
+get_filename_component(SOURCE_DIR "${SOURCE_DIR}" ABSOLUTE)
 set(NUGET_WINDOWS_SDK_BUILD_TOOLS_PATH "${SOURCE_DIR}" PARENT_SCOPE)
