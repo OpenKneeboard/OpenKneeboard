8f18ad11c8d3d24af4c1fb48df6f9b43257c912c
Fred Emmott <git@fredemmott.com>
2022-12-04 13:20:47 -0600

    Add installer artwork

--- assets/rasterize.ps1
+++ assets/rasterize.ps1
@@ -11,65 +11,45 @@ magick convert `
   -trim `
   --% ( -clone 0 -resize 16x16 -extent 16x16 ) ( -clone 0 -resize 32x32 -extent 32x32 ) ( -clone 0 -resize 48x48 -extent 48x48 ) ( -clone 0 -resize 64x64 -extent 64x64 ) ( -clone 0 -resize 128x128 -extent 128x128 ) ( -clone 0 -resize 256x256 -extent 256x256 ) icon.ico
 
-function Create-Png {
-  param (
-    $Size,
-    $FileName
-  )
+# MSI dialog image with left gutter
 
-  magick convert `
-    -background none `
-    "$(Get-Location)\OpenKneeboard_Logos_Icon_Color.svg" `
-    -gravity center `
-    -trim `
-    -resize $Size `
-    -extent $Size `
-    $FileName
-}
+$dialogWidth=493
+$dialogHeight=312
+$gutterWidth=164
+$margin=16
+$overlayWidth=($gutterWidth - (2 * $margin))
 
-function Create-Scaled-Png {
-  param (
-    $NominalSize,
-    $Scale
-  )
-  $ActualSize = $NominalSize * $Scale / 100;
-  Create-Png `
-    "${ActualSize}x${ActualSize}" `
-    "logo-${NominalSize}x${NominalSize}.scale-${Scale}.png"
-}
+magick `
+  -background none `
+  -size ${dialogWidth}x${dialogHeight} canvas:white `
+  -fill '#eee' `
+  -draw "rectangle 0,0, ${gutterWidth},${dialogHeight}" `
+  '(' `
+  "OpenKneeboard_Logos_Flat.svg" `
+  -gravity Center `
+  -trim -resize $overlayWidth `
+  ')' `
+  -gravity NorthWest `
+  -geometry +$margin+$margin `
+  -compose over -composite `
+  WiXUIDialog.png
 
-function Create-Scaled-Png-Set {
-  param (
-    $NominalSize
-  )
+# MSI banner image
 
-  Create-Png `
-    "${NominalSize}x${NominalSize}" `
-    "logo-${NominalSize}x${NominalSize}.png"
-  Create-Scaled-Png $NominalSize 125
-  Create-Scaled-Png $NominalSize 150
-  Create-Scaled-Png $NominalSize 200
-  Create-Scaled-Png $NominalSize 400
-}
+$bannerWidth=$dialogWidth
+$bannerHeight=58
+$iconSize=40
+$margin=(($bannerHeight - $iconSize) / 2)
 
-
-Create-Scaled-Png-Set 44
-Create-Scaled-Png-Set 150
-
-Copy-Item logo-44x44.png logo-44x44.altform-unplated.png
-
-function Create-TargetSize-Png {
-  param (
-    $Size
-  )
-  Create-Png "${Size}x${Size}" "logo-44x44.targetsize-${Size}.png"
-  Copy-Item `
-    "logo-44x44.targetsize-${Size}.png" `
-    "logo-44x44.targetsize-${Size}_altform-unplated.png"
-}
-
-Create-TargetSize-Png 16
-Create-TargetSize-Png 24
-Create-TargetSize-Png 32
-Create-TargetSize-Png 48
-Create-TargetSize-Png 256
+magick `
+  -background none `
+  -size ${bannerWidth}x${bannerHeight} canvas:white `
+  '(' `
+  "OpenKneeboard_Logos_Icon_Flat.svg" `
+  -gravity Center `
+  -trim -resize x$iconSize `
+  ')' `
+  -gravity NorthEast `
+  -geometry +${margin}+$margin `
+  -compose over -composite `
+  WiXUIBanner.png
