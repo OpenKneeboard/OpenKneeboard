7ef0db0fdaa8db10ccb47d3ace242228dc82b924
Fred Emmott <git@fredemmott.com>
2022-12-04 08:49:48 -0600

    Remove old registry keys and ProgramData too

--- installer/RemoveMSIX.ps1
+++ installer/RemoveMSIX.ps1
@@ -1,5 +1,34 @@
 $appx=(Get-AppxPackage -name 'FredEmmott.Self.OpenKneeboard')
-if (!$appx) {
-  return;
+if ($appx) {
+  echo "Uninstalling old MSIX"
+  Remove-AppxPackage $appx
 }
-Remove-AppxPackage $appx
+
+$programData=[Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonApplicationData)
+# Clean up old files...
+$sandboxEscapePath="${programData}\OpenKneeboard"
+if (Test-Path $sandboxEscapePath) {
+  echo "Cleaning up $sandboxEscapePath..."
+  Remove-Item -Recurse $sandboxEscapePath
+}
+
+# Clean up old registry keys
+$subkey="SOFTWARE\Khronos\OpenXR\1\ApiLayers\Implicit"
+$roots = $("HKLM", "HKCU")
+foreach ($root in $roots) {
+  $key="${root}:\${subkey}"
+  if (-not (Test-Path $key)) {
+    continue;
+  }
+
+  $layers=(Get-Item $key).Property
+
+  foreach ($layer in $layers)
+  {
+	  if ($layer -eq "${programData}\OpenKneeboard\OpenKneeboard-OpenXR.json")
+	  {
+      Remove-ItemProperty -Path $key -Name $layer -Force
+      echo "Removed '${layer}' from $key"
+    }
+	}
+}
