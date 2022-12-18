c5f289519639675ec4dc7d1afb696814dab57f32
Fred Emmott <git@fredemmott.com>
2022-12-18 11:29:08 -0600

    Build 32-bit window hook
    Also need a helper exe to inject this
    
    refs #94

---
+++ dualarch.cmake
@@ -0,0 +1,80 @@
+set(
+  DUAL_ARCH_COMPONENTS
+  OpenKneeboard-WindowCaptureHook
+)
+
+if(BUILD_BITNESS EQUAL 32)
+  add_custom_target(OpenKneeboard-dual-arch-components)
+  add_dependencies(OpenKneeboard-dual-arch-components ${DUAL_ARCH_COMPONENTS})
+
+  foreach(TARGET IN LISTS DUAL_ARCH_COMPONENTS)
+    get_target_property(OUTPUT_NAME "${TARGET}" OUTPUT_NAME)
+
+    if(NOT OUTPUT_NAME)
+      set(OUTPUT_NAME "${TARGET}")
+    endif()
+
+    set_target_properties("${TARGET}" PROPERTIES OUTPUT_NAME "${OUTPUT_NAME}32")
+
+    install(TARGETS "${TARGET}" LIBRARY DESTINATION "${CMAKE_INSTALL_BINDIR}" COMPONENT DualArch)
+    install(FILES "$<TARGET_PDB_FILE:${TARGET}>" DESTINATION "." COMPONENT DualArchDebugSymbols)
+  endforeach()
+  return()
+endif()
+
+# 64-bit config here
+foreach(TARGET IN LISTS DUAL_ARCH_COMPONENTS)
+  get_target_property(OUTPUT_NAME "${TARGET}" OUTPUT_NAME)
+
+  if(NOT OUTPUT_NAME)
+    set(OUTPUT_NAME "${TARGET}")
+  endif()
+
+  set_target_properties("${TARGET}" PROPERTIES OUTPUT_NAME "${OUTPUT_NAME}64")
+endforeach()
+
+include(ExternalProject)
+
+ExternalProject_Add(
+  build-32bit-components
+  SOURCE_DIR "${CMAKE_SOURCE_DIR}"
+  DOWNLOAD_COMMAND ""
+  CONFIGURE_COMMAND
+  "${CMAKE_COMMAND}"
+  "${CMAKE_SOURCE_DIR}"
+  -A Win32
+  BUILD_COMMAND
+  "${CMAKE_COMMAND}"
+  --config "$<CONFIG>"
+  --build . --target OpenKneeboard-dual-arch-components
+  INSTALL_COMMAND
+  "${CMAKE_COMMAND}"
+  --install .
+  --prefix "<INSTALL_DIR>/$<CONFIG>"
+  --config "$<CONFIG>"
+  --component DualArch
+  &&
+  "${CMAKE_COMMAND}"
+  --install .
+  --config "$<CONFIG>"
+  --prefix "<INSTALL_DIR>/$<CONFIG>"
+  --component DualArchDebugSymbols
+)
+
+ExternalProject_Get_property(build-32bit-components INSTALL_DIR)
+
+foreach(TARGET IN LISTS DUAL_ARCH_COMPONENTS)
+  add_library("${TARGET}32" INTERFACE)
+  add_dependencies("${TARGET}32" build-32bit-components)
+  set_target_properties(
+    "${TARGET}32"
+    PROPERTIES
+    IMPORTED_LOCATION "${INSTALL_DIR}/$<CONFIG>/bin/${TARGET}32.dll"
+  )
+  install(
+    FILES
+    "${INSTALL_DIR}/$<CONFIG>/${TARGET}32.pdb"
+    DESTINATION "."
+    COMPONENT DebugSymbols
+  )
+endforeach()
