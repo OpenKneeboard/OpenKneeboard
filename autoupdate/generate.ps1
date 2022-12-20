0c232ef3ec3509eb6dd8545399eaa8cb44783aef
Fred Emmott <git@fredemmott.com>
2022-12-20 10:05:30 -0600

    copy-paste, not cut-and-paste

--- autoupdate/generate.ps1
+++ autoupdate/generate.ps1
@@ -12,6 +12,7 @@ $AllReleases
   | Out-File -Encoding utf8NoBOM -FilePath preview-msi.json
 # As above, but only MSIX releases, for compatibility with older autoupdaters
 $AllReleases
+  | Where-Object { $_.assets.Where( {$_.name -Like "*.msix" } ).Count -gt 0 }
   | Select-Object -First 1
   | ConvertTo-Json -Depth 8 -AsArray
   | Out-File -Encoding utf8NoBOM -FilePath preview.json
