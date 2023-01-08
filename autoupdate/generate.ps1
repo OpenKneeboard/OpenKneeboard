657d9efc61d20d8b17b520f202fd8c65ceefdc00
Fred Emmott <git@fredemmott.com>
2023-01-08 09:28:16 -0600

    Add -Stable and -Preview flags to autoupdate generate script
    Also removed code for updating the msix-only autoupdate channels

--- autoupdate/generate.ps1
+++ autoupdate/generate.ps1
@@ -1,32 +1,27 @@
 # This script requires PowerShell 6 or above
+param(
+  [switch] $Preview = $false,
+  [switch] $Stable = $false
+)
 
 # All releases, in version order
 $AllReleases = (Invoke-WebRequest -URI https://api.github.com/repos/OpenKneeboard/OpenKneeboard/releases).Content
 | ConvertFrom-Json
 | Sort-Object -Descending -Property { [System.Management.Automation.SemanticVersion] ($_.tag_name -replace '^v') }
 
-# Latest release, regardless of if it's a prerelease or not
-$AllReleases
-| Select-Object -First 1
-| ConvertTo-Json -Depth 8 -AsArray
-| Out-File -Encoding utf8NoBOM -FilePath preview-msi.json
-# As above, but only MSIX releases, for compatibility with older autoupdaters
-$AllReleases
-| Where-Object { $_.assets.Where( { $_.name -Like "*.msix" } ).Count -gt 0 }
-| Select-Object -First 1
-| ConvertTo-Json -Depth 8 -AsArray
-| Out-File -Encoding utf8NoBOM -FilePath preview.json
+if ($Preview) {
+  # Latest release, regardless of if it's a prerelease or not
+  $AllReleases
+  | Select-Object -First 1
+  | ConvertTo-Json -Depth 8 -AsArray
+  | Out-File -Encoding utf8NoBOM -FilePath preview-msi.json
+}
 
-# Latest stable release
-$AllReleases
-| Where-Object -Not -Property 'prerelease'
-| Select-Object -First 1
-| ConvertTo-Json -Depth 8 -AsArray
-| Out-File -Encoding utf8NoBOM -FilePath stable-msi.json
-# As above, but only MSIX releases, for compatibility with older autoupdaters
-$AllReleases
-| Where-Object { $_.assets.Where( { $_.name -Like "*.msix" } ).Count -gt 0 }
-| Where-Object -Not -Property 'prerelease'
-| Select-Object -First 1
-| ConvertTo-Json -Depth 8 -AsArray
-| Out-File -Encoding utf8NoBOM -FilePath stable.json
+if ($Stable) {
+  # Latest stable release
+  $AllReleases
+  | Where-Object -Not -Property 'prerelease'
+  | Select-Object -First 1
+  | ConvertTo-Json -Depth 8 -AsArray
+  | Out-File -Encoding utf8NoBOM -FilePath stable-msi.json
+}
