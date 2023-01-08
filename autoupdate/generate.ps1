b423861ef7fac7983761ccf0bef8d7d546992a98
Fred Emmott <git@fredemmott.com>
2023-01-08 09:24:15 -0600

    Split out winget to separate script

--- autoupdate/generate.ps1
+++ autoupdate/generate.ps1
@@ -6,8 +6,8 @@ $AllReleases = (Invoke-WebRequest -URI https://api.github.com/repos/OpenKneeboar
 | Sort-Object -Descending -Property { [System.Management.Automation.SemanticVersion] ($_.tag_name -replace '^v') }
 
 # Latest release, regardless of if it's a prerelease or not
-$LatestAny = $AllReleases | Select-Object -First 1
-$LatestAny
+$AllReleases
+| Select-Object -First 1
 | ConvertTo-Json -Depth 8 -AsArray
 | Out-File -Encoding utf8NoBOM -FilePath preview-msi.json
 # As above, but only MSIX releases, for compatibility with older autoupdaters
@@ -30,56 +30,3 @@ $AllReleases
 | Select-Object -First 1
 | ConvertTo-Json -Depth 8 -AsArray
 | Out-File -Encoding utf8NoBOM -FilePath stable.json
-
-function Get-MSIVersion {
-  param (
-    [string] $msiPath
-  )
-
-  $msiPath = (Resolve-Path $msiPath).Path
-
-  $windowsInstaller = New-Object -com WindowsInstaller.Installer
-
-  $database = $windowsInstaller.GetType().InvokeMember(
-    "OpenDatabase", "InvokeMethod", $Null, 
-    $windowsInstaller, @($msiPath, 0)
-  )
-
-  $query = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
-  $view = $database.GetType().InvokeMember(
-    "OpenView", "InvokeMethod", $Null, $database, ($query)
-  )
-
-  $view.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $view, $Null)
-
-  $record = $view.GetType().InvokeMember(
-    "Fetch", "InvokeMethod", $Null, $view, $Null
-  )
-
-  $version = $record.GetType().InvokeMember(
-    "StringData", "GetProperty", $Null, $record, 1
-  )
-
-  $view.GetType().InvokeMember("Close", "InvokeMethod", $Null, $view, $Null)
-
-  return $version
-}
-
-function Update-Winget {
-  param (
-    [Object] $ApiData,
-    [string] $PackageID
-  )
-
-  $uri = "$($ApiData.assets.Where( { $_.name -Like "*.msi" } ).browser_download_url | Select-Object -First 1)"
-  $path = New-TemporaryFile
-  Invoke-WebRequest -Uri $uri -OutFile $path
-
-  wingetcreate update `
-    $PackageID `
-    --out $(Get-Item winget).FullName `
-    --urls $uri `
-    --version $(Get-MSIVersion $path) `
-    --submit
-}
-Update-Winget -ApiData $LatestAny -PackageID FredEmmott.OpenKneeboard.Preview
---
+++ autoupdate/update-winget.ps1
@@ -0,0 +1,66 @@
+param(
+  [switch] $Submit = $false,
+  [switch] $Preview = $false
+)
+
+function Get-MSIVersion {
+  param (
+    [string] $msiPath
+  )
+
+  $msiPath = (Resolve-Path $msiPath).Path
+
+  $windowsInstaller = New-Object -com WindowsInstaller.Installer
+
+  $database = $windowsInstaller.GetType().InvokeMember(
+    "OpenDatabase", "InvokeMethod", $Null, 
+    $windowsInstaller, @($msiPath, 0)
+  )
+
+  $query = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
+  $view = $database.GetType().InvokeMember(
+    "OpenView", "InvokeMethod", $Null, $database, ($query)
+  )
+
+  $view.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $view, $Null)
+
+  $record = $view.GetType().InvokeMember(
+    "Fetch", "InvokeMethod", $Null, $view, $Null
+  )
+
+  $version = $record.GetType().InvokeMember(
+    "StringData", "GetProperty", $Null, $record, 1
+  )
+
+  $view.GetType().InvokeMember("Close", "InvokeMethod", $Null, $view, $Null)
+
+  return $version
+}
+
+function Update-Winget {
+  param (
+    [Object] $ApiData,
+    [string] $PackageID
+  )
+
+  $uri = "$($ApiData.assets.Where( { $_.name -Like "*.msi" } ).browser_download_url | Select-Object -First 1)"
+  $path = New-TemporaryFile
+  Invoke-WebRequest -Uri $uri -OutFile $path
+
+  $extraArgs = @()
+  if ($Submit) {
+    $extraArgs += "--submit"
+  }
+
+  wingetcreate update `
+    $PackageID `
+    --out $(Get-Item winget).FullName `
+    --urls $uri `
+    --version $(Get-MSIVersion $path) `
+    $extraArgs
+}
+
+if ($Preview) {
+  $apiData = Get-Content preview-msi.json | ConvertFrom-Json
+  Update-Winget -ApiData $apiData -PackageID FredEmmott.OpenKneeboard.Preview
+}
