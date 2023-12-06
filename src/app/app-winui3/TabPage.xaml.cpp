613ead97873aac1a1fb675be0bf1ab94bf2aec89
Fred Emmott <git@fredemmott.com>
2023-12-06 12:53:59 -0600

    Adjust in-app mouse input for DPI scaling
    fixes #443

--- src/app/app-winui3/TabPage.xaml.cpp
+++ src/app/app-winui3/TabPage.xaml.cpp
@@ -431,9 +431,13 @@ void TabPage::OnCanvasSizeChanged(
     mSwapChain = nullptr;
     return;
   }
+
+  const auto canvas = Canvas();
+  mCompositionScaleX = canvas.CompositionScaleX();
+  mCompositionScaleY = canvas.CompositionScaleY();
   mCanvasSize = {
-    static_cast<FLOAT>(size.Width) * Canvas().CompositionScaleX(),
-    static_cast<FLOAT>(size.Height) * Canvas().CompositionScaleY(),
+    static_cast<FLOAT>(size.Width) * mCompositionScaleX,
+    static_cast<FLOAT>(size.Height) * mCompositionScaleY,
   };
   const std::unique_lock lock(gDXResources);
   if (mSwapChain) {
@@ -629,6 +633,9 @@ void TabPage::QueuePointerPoint(const PointerPoint& pp) {
   const auto metrics = GetPageMetrics();
   auto x = static_cast<FLOAT>(pp.Position().X);
   auto y = static_cast<FLOAT>(pp.Position().Y);
+
+  x *= mCompositionScaleX;
+  y *= mCompositionScaleY;
 
   x -= metrics.mRenderRect.left;
   y -= metrics.mRenderRect.top;
