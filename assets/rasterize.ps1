dde53266cecd965f53aff8610d7db5ea61d2c10b
Fred Emmott <git@fredemmott.com>
2024-01-05 19:18:19 -0600

    improve icon quality

--- assets/rasterize.ps1
+++ assets/rasterize.ps1
@@ -1,15 +1,23 @@
-# If you're using this as an example for something else and having trouble:
-# imagemagick requires that '(' and ')' are standalone arguments (i.e. distinct
-# entries in argv) - "(foo)" is invalid, but "(" "foo" ")" is valid.
-#
-# Here, I use `--%` to disable powershell's usual argument parsing for the rest
-# of the command
-magick convert `
-  -background none `
-  "$(Get-Location)\OpenKneeboard_Logos_Icon_Color.svg" `
-  -gravity center `
-  -trim `
-  --% ( -clone 0 -resize 16x16 -extent 16x16 ) ( -clone 0 -resize 32x32 -extent 32x32 ) ( -clone 0 -resize 48x48 -extent 48x48 ) ( -clone 0 -resize 64x64 -extent 64x64 ) ( -clone 0 -resize 128x128 -extent 128x128 ) ( -clone 0 -resize 256x256 -extent 256x256 ) icon.ico
+# What imagemagick gives us with default density and whitespace trimmed
+# Using height as we want a square, and our input is taller than it is wide
+$nativeIconHeight = 164
+$nativeDpi = 96
+$iconSizes = 16,32,48,64,128,256
+$intermediates = @()
+foreach ($size in $iconSizes) {
+  $out = "icon-${size}.png"
+  magick convert `
+    -background none `
+    -density $(($nativeDpi * $size) / $nativeIconHeight) `
+    "$(Get-Location)\OpenKneeboard_Logos_Icon_Color.svg" `
+    -trim `
+    -gravity center `
+    -extent "${size}x${size}" `
+    png:$out
+  $intermediates += $out
+}
+magick convert -background none @intermediates icon.ico
+Remove-item $intermediates
 
 # MSI dialog image with left gutter
 
