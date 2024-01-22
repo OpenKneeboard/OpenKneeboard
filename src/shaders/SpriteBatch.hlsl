a1fd15596a33c353249793704cc8d0952663251c
Fred Emmott <git@fredemmott.com>
2024-01-22 07:29:56 -0600

    Also need indexed samplers

--- src/shaders/SpriteBatch.hlsl
+++ src/shaders/SpriteBatch.hlsl
@@ -4,7 +4,7 @@
 // http://go.microsoft.com/fwlink/?LinkId=248929
 
 Texture2D<float4> Textures[] : register(t0);
-sampler TextureSampler : register(s0);
+sampler TextureSamplers[] : register(s0);
 
 void SpriteVertexShader(
     inout uint textureIndex : TEXTURE_INDEX,
@@ -15,11 +15,24 @@ void SpriteVertexShader(
     // Nothing to do, no transformation matrices here.
 }
 
+float4 SpritePixelShader_impl(
+    Texture2D<float4> texture,
+    sampler textureSampler,
+    float4 color      : COLOR0,
+    float2 texCoord   : TEXCOORD0) : SV_Target0
+{
+    return texture.Sample(textureSampler, texCoord) * color;
+}
+
 
 float4 SpritePixelShader(
     uint textureIndex : TEXTURE_INDEX,
     float4 color      : COLOR0,
     float2 texCoord   : TEXCOORD0) : SV_Target0
 {
-    return Textures[NonUniformResourceIndex(textureIndex)].Sample(TextureSampler, texCoord) * color;
+    return SpritePixelShader_impl(
+        Textures[NonUniformResourceIndex(textureIndex)],
+        TextureSamplers[NonUniformResourceIndex(textureIndex)],
+        color,
+        texCoord);
 }
