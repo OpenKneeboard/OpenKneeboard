d8f9810608a555340e040a4f1665fe5fda0fec61
Fred Emmott <git@fredemmott.com>
2024-01-27 19:42:20 -0600

    Remove unneeded NonUniformResourceIndex from shader

--- src/shaders/SpriteBatch.hlsl
+++ src/shaders/SpriteBatch.hlsl
@@ -40,5 +40,5 @@ float4 SpritePixelShader(
     float4 color      : COLOR0,
     float2 texCoord   : TEXCOORD0,
     uint textureIndex : TEXTURE_INDEX) : SV_Target0 {
-    return Textures[NonUniformResourceIndex(textureIndex)].Sample(TextureSampler, texCoord) * color;
+    return Textures[textureIndex].Sample(TextureSampler, texCoord) * color;
 }
