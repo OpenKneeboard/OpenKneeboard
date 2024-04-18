3dfe2ccf062073d764f31855ac865fe807b7f85b
Fred Emmott <git@fredemmott.com>
2024-04-18 15:23:08 -0500

    Use msbuild's clang-tidy support to generate a compile_commands.json

---
+++ scripts/make-compile-commands.ps1
@@ -0,0 +1,32 @@
+# Copyright (C) 2024 Fred Emmott <fred@fredemmott.com>
+# SPDX-License-Identifier: ISC
+
+$wasCreated = $false;
+$mutex = New-Object System.Threading.Mutex($true, "com.fredemmott.merge-compile-commands", [ref] $wasCreated);
+if (!$wasCreated) {
+  $mutex.WaitOne() | Out-Null;
+}
+
+$Folders = Get-ChildItem -Path . -Filter '*.ClCompile_flags.txt' -Recurse | % { Split-Path -parent $_.FullName } | Get-Unique
+
+foreach ($Folder in $Folders) {
+  $FlagFiles = Get-ChildItem -Path $Folder -Filter '*.ClCompile_flags.txt' | % { $_.FullName }
+  $Entries = @()
+  foreach ($FlagFile in $FlagFiles) {
+    $SourcesFile = $FlagFile.Replace('ClCompile_flags.txt', 'ClCompile_sources.txt')
+    if (!(Test-Path $SourcesFile)) {
+      continue;
+    }
+    $Command = "`"C:\WINDOWS\system32\CL.exe`" $(Get-Content $FlagFile)"
+    foreach ($Source in $(Get-Content $SourcesFile)) {
+      $Entries += @{
+        directory = $Folder;
+        command   = "$command `"$Source`"";
+        file      = "$Source";
+      };
+    }
+  }
+  ConvertTo-Json $Entries | Set-Content -Path "$Folder/compile_commands.json" -Encoding UTF8
+}
+
+$mutex.ReleaseMutex();
