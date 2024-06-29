ace674ab260ac25706a179662be2fd2918dd7da8
Fred Emmott <git@fredemmott.com>
2024-06-29 12:15:19 -0500

    Log temporary directory on startup

--- src/app/app-winui3/App.xaml.cpp
+++ src/app/app-winui3/App.xaml.cpp
@@ -22,6 +22,8 @@
 #include "App.xaml.h"
 // clang-format on
 
+#include <OpenKneeboard/config.h>
+
 #include "Globals.h"
 #include "MainWindow.xaml.h"
 
@@ -38,7 +40,6 @@
 #include <OpenKneeboard/WebView2PageSource.h>
 #include <OpenKneeboard/Win32.h>
 
-#include <OpenKneeboard/config.h>
 #include <OpenKneeboard/dprint.h>
 #include <OpenKneeboard/scope_guard.h>
 #include <OpenKneeboard/tracing.h>
@@ -204,6 +205,9 @@ void App::OnLaunched(LaunchActivatedEventArgs const&) noexcept {
 static void LogSystemInformation() {
   dprintf("{} {}", ProjectReverseDomainA, Version::ReleaseName);
   dprintf(L"Full path: {}", GetFullPathForCurrentExecutable());
+  dprintf(
+    L"Temporary directory: {}",
+    OpenKneeboard::Filesystem::GetTemporaryDirectory());
   dprint("----------");
   dprintf("  Elevated: {}", IsElevated());
   dprintf("  Shell Elevated: {}", IsShellElevated());
