cdec6ae0284de051eede66bae57ce3076f591af1
Fred Emmott <git@fredemmott.com>
2024-08-03 17:55:56 -0500

    Fix context_binder::operator() outliving the context_binder()

--- src/app/app-common/include/OpenKneeboard/weak_refs/cppwinrt/bind_context.hpp
+++ src/app/app-common/include/OpenKneeboard/weak_refs/cppwinrt/bind_context.hpp
@@ -30,13 +30,14 @@
 
 namespace OpenKneeboard::weak_refs::cppwinrt_detail {
 template <class TContext, class TFn>
-struct context_binder {
+struct context_binder_inner
+  : std::enable_shared_from_this<context_binder_inner<TContext, TFn>> {
  public:
   using function_t = TFn;
 
-  context_binder() = delete;
+  context_binder_inner() = delete;
   template <class InitContext, class InitFn>
-  context_binder(InitContext&& context, InitFn&& fn)
+  context_binder_inner(InitContext&& context, InitFn&& fn)
     : mContext(std::forward<InitContext>(context)),
       mFn(std::forward<InitFn>(fn)) {
   }
@@ -46,19 +47,44 @@ struct context_binder {
   template <class... UnboundArgs>
     requires std::invocable<TFn, UnboundArgs...>
   winrt::fire_and_forget operator()(UnboundArgs... unboundArgs) const {
+    auto weak = this->weak_from_this();
+    auto ctx = mContext;
     if constexpr (std::
                     same_as<std::decay_t<TContext>, winrt::apartment_context>) {
-      co_await mContext;
+      co_await ctx;
     } else {
-      co_await wil::resume_foreground(mContext);
+      co_await wil::resume_foreground(ctx);
     }
-    std::invoke(mFn, unboundArgs...);
+    if (auto self = weak.lock()) {
+      std::invoke(mFn, unboundArgs...);
+    }
   }
 
  private:
   TContext mContext;
   TFn mFn;
 };
+
+template <class TContext, class TFn>
+struct context_binder_outer {
+  using function_t = TFn;
+
+  context_binder_outer() = delete;
+  template <class... Args>
+  context_binder_outer(Args&&... args) {
+    mInner = std::make_shared<context_binder_inner<TContext, TFn>>(
+      std::forward<Args>(args)...);
+  }
+
+  template <class... UnboundArgs>
+  void operator()(UnboundArgs&&... unboundArgs) const {
+    std::invoke(*mInner, std::forward<UnboundArgs>(unboundArgs)...);
+  }
+
+ private:
+  std::shared_ptr<context_binder_inner<TContext, TFn>> mInner;
+};
+
 }// namespace OpenKneeboard::weak_refs::cppwinrt_detail
 
 namespace OpenKneeboard::weak_refs::cppwinrt {
@@ -66,7 +92,7 @@ namespace OpenKneeboard::weak_refs::cppwinrt {
 template <class Context, class F>
 auto bind_context(Context&& context, F&& f) {
   return cppwinrt_detail::
-    context_binder<std::decay_t<Context>, std::decay_t<F>> {
+    context_binder_outer<std::decay_t<Context>, std::decay_t<F>> {
       std::forward<Context>(context), std::forward<F>(f)};
 }
 
