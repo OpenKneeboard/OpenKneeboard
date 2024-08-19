0d8f53e1072b55d97772d67c864f43e46bc5d73e
Fred Emmott <git@fredemmott.com>
2024-08-19 07:59:22 -0500

    Update make-compile-commands to build with clang-tidy from clang19

--- scripts/make-compile-commands.in.ps1
+++ scripts/make-compile-commands.in.ps1
@@ -6,23 +6,50 @@ $extraArgs = @(
   "/D_XM_NO_INTRINSICS_",
   # Mostly used for TraceLogging fun
   "/DCLANG_TIDY=1"
+  # Fixup: As of 2024-08-19, C++23 in MSVC needs /std:c++latest, but we need --std=c++23 for clang-tidy
+  "/clang:--std=c++@CMAKE_CXX_STANDARD@"
+  # Spammy or irrelevant
+  "/clang:-Wno-unknown-pragmas"
+  "/clang:-Wno-missing-braces"
+  "/clang:-Wno-vexing-parse"
+)
+
+$removeArgs = @(
+  # Other half of the /clang:--std=c++ above
+  "/std:c++latest"
+  "/std:c++@CMAKE_CXX_STANDARD@" 
+  # This means 'C++'; clang-cl + clang-tidy adds this, which leads to clang-diagnostic-overriding-option
+  "/TP"
 )
 
 $Entries = @()
-$Folders = Get-ChildItem -Path . -Filter '*.ClCompile_flags.txt' -Recurse | % { Split-Path -parent $_.FullName } | Get-Unique
+$Folders = Get-ChildItem -Path . -Filter '*.ClCompile_flags.txt' -Recurse | ForEach-Object { Split-Path -parent $_.FullName } | Get-Unique
 
 foreach ($Folder in $Folders) {
-  $FlagFiles = Get-ChildItem -Path $Folder -Filter '*.ClCompile_flags.txt' | % { $_.FullName }
+  $FlagFiles = Get-ChildItem -Path $Folder -Filter '*.ClCompile_flags.txt' | ForEach-Object { $_.FullName }
   foreach ($FlagFile in $FlagFiles) {
     $SourcesFile = $FlagFile.Replace('ClCompile_flags.txt', 'ClCompile_sources.txt')
     if (!(Test-Path $SourcesFile)) {
       continue;
     }
-    $Command = "`"@NATIVE_PATH_CMAKE_CXX_COMPILER@`" $($extraArgs -join ' ') $(Get-Content $FlagFile)"
+
+    $Flags = Get-Content $FlagFile
+    if (!$Flags) {
+      continue;
+    }
+
+    # ~ August 2024, MSBuild has started adding a semicolon before additional flags
+    $compileArgs = Invoke-Expression "echo $($Flags -replace ';','')";
+    $compileArgs = $compileArgs | Where-Object { $_ -notin $removeArgs };
+    $compileArgs += $extraArgs;
+
+    $compileArgs = $compileArgs | Join-String -DoubleQuote -Separator ' '
+
+    $Command = "`"@NATIVE_PATH_CMAKE_CXX_COMPILER@`" $($compileArgs)"
     foreach ($Source in $(Get-Content $SourcesFile)) {
       $Entries += @{
         directory = $Folder;
-        command   = "$command `"$Source`"";
+        command   = "$Command `"$Source`"";
         file      = "$Source";
       };
     }
