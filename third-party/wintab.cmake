920385290100dbfd41bfcbc9c872a318e16c39ea
Fred Emmott <git@fredemmott.com>
2024-08-20 11:46:44 -0500

    [clang-tidy] Mark wintab as SYSTEM

--- third-party/wintab.cmake
+++ third-party/wintab.cmake
@@ -1,5 +1,5 @@
 add_library(wintab INTERFACE)
-target_include_directories(wintab INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/wintab/include")
+target_include_directories(wintab SYSTEM INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/wintab/include")
 
 add_library(ThirdParty::Wintab ALIAS wintab)
 
