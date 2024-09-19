634c287d7bc370d37e8a40a4b5a9195d8c71ddca
Fred Emmott <git@fredemmott.com>
2024-09-19 16:29:39 -0500

    🗑️ Remove example plugin
    Here's a better one: https://github.com/OpenKneeboard/calculator

--- plugin-example/package.ps1
+++
@@ -1,8 +0,0 @@
-$Out = "Test.OpenKneeboardPlugin";
-$Zip = "$Out.zip"
-Remove-Item $Out
-if (Test-Path $Zip) {
-    Remove-Item $Zip
-}
-Compress-Archive v1.json -DestinationPath $Zip
-Rename-Item $Zip $Out
