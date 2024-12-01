f8d2056956d444f3a639894487a68119f702d171
Fred Emmott <git@fredemmott.com>
2024-12-01 12:48:33 -0600

    🩹 conformance: return XR_ERROR_LIMIT_REACHED if we already have a session
    Detected by 'multithreading' in CTS

--- src/injectables/OpenXRKneeboard.cpp
+++ src/injectables/OpenXRKneeboard.cpp
@@ -485,7 +485,7 @@ XrResult xrCreateSession(
 
   if (gKneeboard) {
     dprint("Already have a kneeboard, refusing to initialize twice");
-    return XR_ERROR_INITIALIZATION_FAILED;
+    return XR_ERROR_LIMIT_REACHED;
   }
 
   const auto system = createInfo->systemId;
