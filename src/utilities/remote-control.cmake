797e842cf7a88c2e82d83b78acc72b0db9648340
Fred Emmott <git@fredemmott.com>
2025-12-31 09:27:21 -0600

    ♻️ Rewrite simple-remote using `magic_args`
    - Much less build system magic
    - Use the enums rather than the strings
    - Single binary. Saves a little space, but mostly cuts down on the amount of times I need to enter my PIN when signing releases

---
+++ src/utilities/remote-control.cmake
@@ -0,0 +1,23 @@
+find_package(magic_args CONFIG REQUIRED)
+include(MagicArgs)
+
+add_utility_executable(
+  OpenKneeboard-RemoteControl
+  WIN32
+  simple-remote.cpp
+  remote-traceprovider.cpp
+)
+target_link_libraries(
+  OpenKneeboard-RemoteControl
+  PRIVATE
+  OpenKneeboard-APIEvent
+  OpenKneeboard-UserAction
+  OpenKneeboard-dprint
+  OpenKneeboard-tracing
+  magic_args::magic_args
+)
+magic_args_enumerate_subcommands(
+  OpenKneeboard-RemoteControl
+  HARDLINKS_DIR "$<TARGET_FILE_DIR:OpenKneeboard-RemoteControl>"
+  TEXT_FILE "$<TARGET_FILE_DIR:OpenKneeboard-RemoteControl>/$<TARGET_FILE_BASE_NAME:OpenKneeboard-RemoteControl>-aliases.txt"
+)
--- src/utilities/simple-remote.cpp
+++ src/utilities/simple-remote.cpp
@@ -2,64 +2,130 @@
 //
 // Copyright (c) 2025 Fred Emmott <fred@fredemmott.com>
 //
-// This program is open source; see the LICENSE file in the root of the OpenKneeboard repository.
+// This program is open source; see the LICENSE file in the root of the
+// OpenKneeboard repository.
 #include <OpenKneeboard/APIEvent.hpp>
-
-using OpenKneeboard::APIEvent;
-
-#define STRINGIFY_INNER(x) #x
-#define STRINGIFY(x) STRINGIFY_INNER(x)
-
-#include <Windows.h>
+#include <OpenKneeboard/UserAction.hpp>
+#include <OpenKneeboard/Win32.hpp>
 
 #include <OpenKneeboard/dprint.hpp>
 
+#include <windows.h>
+
+#include <magic_args/magic_args.hpp>
+#include <magic_args/subcommands.hpp>
+
 #include <filesystem>
 
-#include <shellapi.h>
+using namespace OpenKneeboard;
 
-// We only need a standard `main()` function, but using wWinMain prevents
-// a window/task bar entry from temporarily appearing
-int WINAPI wWinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  PWSTR pCmdLine,
-  int nCmdShow) {
-  int argc = 0;
-  auto argv = CommandLineToArgvW(pCmdLine, &argc);
+template <UserAction TAction>
+struct SimpleRemote {
+  static constexpr auto action = TAction;
 
-  int repeat = 1;
+  struct arguments_type {
+    magic_args::optional_positional_argument<std::size_t> mCount {
+      .storage = 1,// default
+      .help = "Number of times to perform the action",
+    };
+  };
 
-  wchar_t* wpgmptr {nullptr};
-  _get_wpgmptr(&wpgmptr);
-  // argv[0] is sometimes the first arg, sometimes the program name
-  if (
-    std::filesystem::weakly_canonical(argv[0])
-    != std::filesystem::weakly_canonical(wpgmptr)) {
-    try {
-      repeat = std::stoi(argv[0]);
-    } catch (...) {
+  static int main(const arguments_type& args) {
+    static const std::string ActionString {magic_enum::enum_name(TAction)};
+
+    const auto count = *args.mCount;
+
+    dprint("Remote invoked: {} (count: {})", ActionString, count);
+
+    switch (count) {
+      case 0:
+        return EXIT_SUCCESS;
+      case 1:
+        APIEvent {APIEvent::EVT_REMOTE_USER_ACTION, ActionString}.Send();
+        return EXIT_SUCCESS;
+      default: {
+        const auto single = nlohmann::json::array(
+          {APIEvent::EVT_REMOTE_USER_ACTION, ActionString});
+
+        auto payload = nlohmann::json::array();
+        for (std::size_t i = 0; i < count; ++i) {
+          payload.push_back(single);
+        }
+        APIEvent {APIEvent::EVT_MULTI_EVENT, payload.dump()}.Send();
+        return EXIT_SUCCESS;
+      }
     }
   }
+};
 
-  if (repeat == 1) {
-    const APIEvent ge {
-      APIEvent::EVT_REMOTE_USER_ACTION,
-      STRINGIFY(REMOTE_ACTION),
-    };
-    ge.Send();
-    return 0;
+namespace SimpleRemotes {
+template <UserAction... Actions>
+using make = magic_args::invocable_subcommands_list<SimpleRemote<Actions>...>;
+
+using enum UserAction;
+using subcommands = make<
+  CYCLE_ACTIVE_VIEW,
+  DECREASE_BRIGHTNESS,
+  DISABLE_TINT,
+  ENABLE_TINT,
+  HIDE,
+  INCREASE_BRIGHTNESS,
+  NEXT_BOOKMARK,
+  NEXT_PAGE,
+  NEXT_PROFILE,
+  NEXT_TAB,
+  PREVIOUS_BOOKMARK,
+  PREVIOUS_PAGE,
+  PREVIOUS_PROFILE,
+  PREVIOUS_TAB,
+  RECENTER_VR,
+  REPAINT_NOW,
+  SHOW,
+  SWAP_FIRST_TWO_VIEWS,
+  TOGGLE_BOOKMARK,
+  TOGGLE_FORCE_ZOOM,
+  TOGGLE_TINT,
+  TOGGLE_VISIBILITY>;
+};// namespace SimpleRemotes
+
+struct RootInfo {
+  using subcommands = SimpleRemotes::subcommands;
+
+  static int main(
+    const std::expected<
+      int,
+      magic_args::subcommands_winmain_unexpected_t<subcommands>>& exitCode,
+    HINSTANCE,
+    [[maybe_unused]] const int nCmdShow) {
+    if (exitCode) [[likely]] {
+      return *exitCode;
+    }
+
+    using OpenKneeboard::Win32;
+    const auto message = Win32::UTF8::to_wide(exitCode.error().output);
+    const auto title
+      = std::filesystem::path {Win32::Wide::or_throw::GetModuleFileName()}
+          .stem()
+          .wstring();
+
+    MessageBoxW(
+      nullptr,
+      message->c_str(),
+      title.c_str(),
+      MB_OK | (is_error(exitCode.error()) ? MB_ICONERROR : MB_ICONINFORMATION));
+
+    return is_error(exitCode.error()) ? EXIT_FAILURE : EXIT_SUCCESS;
   }
 
-  auto payload = nlohmann::json::array();
-  for (int i = 0; i < repeat; ++i) {
-    payload.push_back(nlohmann::json::array(
-      {APIEvent::EVT_REMOTE_USER_ACTION, STRINGIFY(REMOTE_ACTION)}));
+  template <class T, auto Name>
+  static consteval auto normalize_subcommand_name() {
+    constexpr std::string_view prefix {"OpenKneeboard-RemoteControl-"};
+    constexpr std::string_view suffix {magic_enum::enum_name<T::action>()};
+    std::array<char, prefix.size() + suffix.size()> ret;
+    auto begin = std::ranges::copy(prefix, ret.begin()).out;
+    std::ranges::copy(suffix, begin);
+    return ret;
   }
-  const APIEvent me {
-    APIEvent::EVT_MULTI_EVENT,
-    payload.dump(),
-  };
-  me.Send();
-  return 0;
-}
+};
+
+MAGIC_ARGS_MULTICALL_WINMAIN(RootInfo)
