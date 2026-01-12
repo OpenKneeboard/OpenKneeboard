b279e50d4b06c34b9b6a652adf97b01f00b52d95
Fred Emmott <git@fredemmott.com>
2026-01-12 06:07:05 -0600

    ⬆️ Update QPDF to v12.3
    https://github.com/qpdf/qpdf/issues/1636 appears resolved :)

--- third-party/vcpkg-overlays/ports/qpdf/portfile.cmake
+++ third-party/vcpkg-overlays/ports/qpdf/portfile.cmake
@@ -1,30 +1,30 @@
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO qpdf/qpdf
-    REF v${VERSION}
-    SHA512 9c57078b9fdfcbb1fb3330fac9b5f01279617a0d87c11a4cc01884e199f9936e49d9f7c9e88ac5fbb0ff660194017fc6218bb8929eba9ebd5b4ce56099a937bf
-    PATCHES
-        cmake-library-only.patch
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO qpdf/qpdf
+  REF v${VERSION}
+  SHA512 eed4c063bcb9e0fddfe03d16c63902f2552aa987e9d8048a592e677e43caf035ed1231caf509d4b2e7391593d72f9493156129c522b4022d3633ed62784c2338
+  PATCHES
+  cmake-library-only.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        gnutls         REQUIRE_CRYPTO_GNUTLS
-        openssl        REQUIRE_CRYPTO_OPENSSL
-        zopfli         ZOPFLI
+  FEATURES
+  gnutls REQUIRE_CRYPTO_GNUTLS
+  openssl REQUIRE_CRYPTO_OPENSSL
+  zopfli ZOPFLI
 )
 
 vcpkg_find_acquire_program(PKGCONFIG)
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        ${FEATURE_OPTIONS}
-        -DREQUIRE_CRYPTO_NATIVE=ON
-        -DUSE_IMPLICIT_CRYPTO=OFF
-        -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+  ${FEATURE_OPTIONS}
+  -DREQUIRE_CRYPTO_NATIVE=ON
+  -DUSE_IMPLICIT_CRYPTO=OFF
+  -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
 )
 
 vcpkg_cmake_install()
