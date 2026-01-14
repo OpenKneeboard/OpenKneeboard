1c8c41ed15dbadf977f4254d940d8ba8b0b226c5
Fred Emmott <git@fredemmott.com>
2026-01-14 08:41:33 -0600

    🔊 Add main process VRAM usage to debug logs
    Does not include Chromium subprocesses; does not seem to be a reasonable way to get chromium VRAM when using ANGLE, which appears needed to force the LUID

--- src/app/app-winui3/HelpPage.xaml.h
+++ src/app/app-winui3/HelpPage.xaml.h
@@ -2,7 +2,8 @@
 //
 // Copyright (c) 2025 Fred Emmott <fred@fredemmott.com>
 //
-// This program is open source; see the LICENSE file in the root of the OpenKneeboard repository.
+// This program is open source; see the LICENSE file in the root of the
+// OpenKneeboard repository.
 #pragma once
 
 // clang-format off
@@ -51,6 +52,7 @@ struct HelpPage : HelpPageT<HelpPage>,
   static std::string GetUpdateLog() noexcept;
   static std::string GetOpenXRInfo() noexcept;
   static std::string GetActiveConsumers() noexcept;
+  static std::string GetVRAMInfo() noexcept;
 
   void DisplayLicense(const std::string& header, const std::filesystem::path&);
 
