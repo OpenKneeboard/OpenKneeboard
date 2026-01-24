0e6fcc29a903fd45ef9ef098defb856acd189169
Fred Emmott <git@fredemmott.com>
2026-01-24 12:14:58 -0600

    ✨ add audited_weak_ptr
    Also strengthen the concurrency safety

--- src/lib/include/OpenKneeboard/audited_ptr.hpp
+++ src/lib/include/OpenKneeboard/audited_ptr.hpp
@@ -8,11 +8,16 @@
 
 #include <OpenKneeboard/dprint.hpp>
 
+#include <felly/guarded_data.hpp>
+
 #include <atomic>
 #include <source_location>
 #include <unordered_map>
 
 namespace OpenKneeboard {
+
+template <class T>
+class audited_weak_ptr;
 
 /// Wraps std::shared_ptr to add accounting, to track down leaks
 template <class T>
@@ -22,11 +27,20 @@ class audited_ptr {
 
   struct RefData {
     std::unordered_map<uint64_t, std::source_location> mRefs;
-    std::atomic_uint64_t mNextID {0};
+    uint64_t mNextID {0};
+
+    [[nodiscard]]
+    uint64_t AddRef(const std::source_location& loc) {
+      const auto id = mNextID++;
+      mRefs.emplace(id, loc);
+      return id;
+    }
+
+    void Release(const uint64_t id) { mRefs.erase(id); }
   };
 
   std::shared_ptr<T> mImpl;
-  std::shared_ptr<RefData> mRefData;
+  std::shared_ptr<felly::guarded_data<RefData>> mRefData;
 
  public:
   audited_ptr() = default;
@@ -36,10 +50,11 @@ class audited_ptr {
     T* ptr,
     const std::source_location& loc = std::source_location::current()) {
     mImpl = std::shared_ptr<T>(ptr);
-    mRefData = std::make_shared<RefData>();
+    RefData refData {};
+    mRefID = refData.AddRef(loc);
 
-    mRefID = mRefData->mNextID++;
-    mRefData->mRefs.emplace(mRefID, loc);
+    mRefData =
+      std::make_shared<felly::guarded_data<RefData>>(std::move(refData));
   }
 
   audited_ptr(
@@ -48,18 +63,26 @@ class audited_ptr {
     copy_from(other, loc);
   }
 
-  audited_ptr(
-    audited_ptr&& other,
-    const std::source_location& loc =
-      std::source_location::current()) noexcept {
-    copy_from(other, loc);
+  audited_ptr(audited_ptr&&) noexcept = default;
+  audited_ptr& operator=(audited_ptr&& other) noexcept {
+    if (this == std::addressof(other)) {
+      return *this;
+    }
+
+    if (mRefData) {
+      mRefData->lock()->Release(mRefID);
+    }
+    mImpl = std::move(other.mImpl);
+    mRefData = std::move(other.mRefData);
+    mRefID = std::exchange(other.mRefID, ~(0ui64));
+    return *this;
   }
 
   ~audited_ptr() noexcept {
     if (!mRefData) {
       return;
     }
-    mRefData->mRefs.erase(mRefID);
+    mRefData->lock()->Release(mRefID);
   }
 
   T* get() const noexcept { return mImpl.get(); }
@@ -74,12 +97,12 @@ class audited_ptr {
   }
 
   audited_ptr& operator=(const audited_ptr&) = delete;
-  audited_ptr& operator=(audited_ptr&&) = delete;
 
   auto use_count() const { return mImpl.use_count(); }
 
   void dump_refs(std::string_view debugLabel) const {
-    const auto refs = mRefData->mRefs;
+    const auto refData = mRefData->lock();
+    const auto& refs = refData->mRefs;
     dprint("DEBUG: {} references remaining to `{}`", refs.size(), debugLabel);
     for (const auto& [id, loc]: refs) {
       dprint(
@@ -95,7 +118,7 @@ class audited_ptr {
     const audited_ptr& other,
     const std::source_location& loc = std::source_location::current()) {
     if (mRefData) {
-      mRefData->mRefs.erase(mRefID);
+      mRefData->lock()->Release(mRefID);
     }
 
     mImpl = other.mImpl;
@@ -103,14 +126,42 @@ class audited_ptr {
     mRefID = ~(0ui64);
 
     if (mRefData) {
-      mRefID = mRefData->mNextID++;
-      mRefData->mRefs.emplace(mRefID, loc);
+      mRefID = mRefData->lock()->AddRef(loc);
     }
 
     return *this;
   }
 
   operator bool() const { return !!mImpl; }
+
+  friend class audited_weak_ptr<T>;
+};
+
+template <class T>
+class audited_weak_ptr {
+ public:
+  constexpr audited_weak_ptr() = default;
+  audited_weak_ptr(const audited_ptr<T>& ptr)
+    : mImpl(ptr.mImpl),
+      mRefData(ptr.mRefData) {}
+  audited_ptr<T> lock(
+    const std::source_location& loc = std::source_location::current()) {
+    auto shared = mImpl.lock();
+    if (!shared) {
+      return {};
+    }
+    audited_ptr<T> ret;
+    ret.mImpl = std::move(shared);
+    ret.mRefData = mRefData;
+    if (mRefData) {
+      ret.mRefID = mRefData->lock()->AddRef(loc);
+    }
+    return std::move(ret);
+  }
+
+ private:
+  std::weak_ptr<T> mImpl;
+  decltype(audited_ptr<T>::mRefData) mRefData;
 };
 
 }// namespace OpenKneeboard
