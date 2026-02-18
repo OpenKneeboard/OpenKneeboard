169ec6288fad1a2b7170a6d0705c9729a35476ac
Fred Emmott <git@fredemmott.com>
2026-02-18 10:17:27 -0600

    🔨 Stop using VCPKG_APPLOCAL_DEPS
    Explicitly copy lualib in
    
    Use generator expressions for absolute path to support sources that are generator expressions themselves.

--- cmake/ok_add_runtime_files.cmake
+++ cmake/ok_add_runtime_files.cmake
@@ -1,11 +1,13 @@
 include_guard(GLOBAL)
 
 function(ok_add_runtime_files TARGET)
+  if (NOT ENABLE_APP)
+    return()
+  endif ()
   set(options)
   set(valueArgs DESTINATION)
   set(multiValueArgs)
   cmake_parse_arguments(ARG "${options}" "${valueArgs}" "${multiValueArgs}" ${ARGN})
-
 
   set(DEST_SUBDIR bin)
   if (ARG_DESTINATION)
@@ -14,22 +16,19 @@ function(ok_add_runtime_files TARGET)
   set(DEST "${BUILD_OUT_ROOT}/${DEST_SUBDIR}")
 
   set(SOURCES)
-  set(OUTPUTS)
   foreach (SOURCE IN LISTS ARG_UNPARSED_ARGUMENTS)
-    cmake_path(ABSOLUTE_PATH SOURCE BASE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
-    list(APPEND SOURCES "${SOURCE}")
-
-    cmake_path(GET SOURCE FILENAME FILENAME)
-    list(APPEND OUTPUTS "${DEST}/${FILENAME}")
+    list(APPEND SOURCES "$<PATH:ABSOLUTE_PATH,${SOURCE},${CMAKE_CURRENT_SOURCE_DIR}>")
   endforeach ()
 
+  set(STAMP_FILE "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.stamp")
   add_custom_command(
-    OUTPUT "${OUTPUTS}"
+    OUTPUT "${STAMP_FILE}"
     DEPENDS "${SOURCES}"
     COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${SOURCES}" "${DEST}/"
+    COMMAND "${CMAKE_COMMAND}" -E touch "${STAMP_FILE}"
     VERBATIM
     COMMAND_EXPAND_LISTS
   )
-  add_custom_target(${TARGET} DEPENDS "${OUTPUTS}")
+  add_custom_target(${TARGET} DEPENDS "${STAMP_FILE}")
   add_dependencies(runtime-dependencies "${TARGET}")
 endfunction()
--- third-party/lualib.cmake
+++ third-party/lualib.cmake
@@ -5,3 +5,20 @@ add_library(lualib_delayload INTERFACE)
 target_link_libraries(lualib_delayload INTERFACE lua)
 target_link_options(lualib_delayload INTERFACE "/DELAYLOAD:$<TARGET_FILE_NAME:lua>")
 add_library(ThirdParty::Lua::DelayLoad ALIAS lualib_delayload)
+
+get_target_property(
+  LUA_RELEASE_DLL
+  lua
+  IMPORTED_LOCATION_RELEASE
+)
+get_target_property(
+  LUA_DEBUG_DLL
+  lua
+  IMPORTED_LOCATION_DEBUG
+)
+
+include(ok_add_runtime_files)
+ok_add_runtime_files(
+  lua-runtime-dll
+  "$<IF:$<CONFIG:Debug>,${LUA_DEBUG_DLL},${LUA_RELEASE_DLL}>"
+)
